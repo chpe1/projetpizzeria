5fa68e0fee8765152f7f2bc40a8403500cf2d85a
Jerome Noiret <jeromenoiret@free.fr>
2021-07-08 13:28:58 +0200

    create api

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Pizza
 
 # Create your views here.
@@ -11,3 +12,8 @@ def index(request):
     # pizzas_names_and_prices_str = ", ".join(pizzas_names_and_prices)
     # return HttpResponse("Les pizzas : " + pizzas_names_and_prices_str)
     return render(request, 'menu/index.html', {'pizzas' : pizzas})
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by('prix')
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
